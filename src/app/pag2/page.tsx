3d64e5c11b0f2cff0fd12ce53a8a263e8da9f3b6
Lucy Murray <lucyamurray@me.com>
2024-05-09 09:20:16 +0100

    add header and footer

---
+++ src/app/pag2/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Typography from '@mui/material/Typography';
+import { Button } from '@mui/material';
+
+const Pag2: React.FC = () => {
+    return (
+        <div>
+            <Typography variant="h1">Hello, Pag2!</Typography>
+            <Button
+                variant="contained"
+                color="primary">Click me</Button>
+        </div>
+    );
+};
+
+export default Pag2;
